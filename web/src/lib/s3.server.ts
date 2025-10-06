148a6a8a0f75e82b7cb9b05cd3c515d196cc26cc
Arti <59352535+artifishvr@users.noreply.github.com>
2025-10-05 18:42:46 -0700

    [web] use aws4fetch

--- web/src/lib/s3.server.ts
+++ web/src/lib/s3.server.ts
@@ -1,11 +1,20 @@
 import { S3_BUCKET, S3_ENDPOINT, S3_ACCESS_KEY, S3_ACCESS_SECRET } from '$env/static/private';
-import { S3Client } from '@aws-sdk/client-s3';
+import { AwsClient } from 'aws4fetch';
 
-export const s3 = new S3Client({
-	region: 'auto',
-	endpoint: S3_ENDPOINT,
-	credentials: {
-		accessKeyId: S3_ACCESS_KEY || '',
-		secretAccessKey: S3_ACCESS_SECRET || ''
-	}
+const endpoint = S3_ENDPOINT || 'https://dvr-storage.vrc.bz';
+
+export const aws = new AwsClient({
+	accessKeyId: S3_ACCESS_KEY || '',
+	secretAccessKey: S3_ACCESS_SECRET || '',
+	service: 's3',
+	region: 'auto'
 });
+
+export const S3 = {
+	async listObjects(): Promise<string> {
+		const url = `${endpoint}/${S3_BUCKET}`;
+		const res = await aws.fetch(url + '?list-type=2');
+		if (!res.ok) throw new Error(`S3 listObjects failed: ${res.status} ${res.statusText}`);
+		return res.text();
+	}
+};
--- web/src/routes/+page.server.ts
+++ web/src/routes/+page.server.ts
@@ -1,29 +1,29 @@
-import { ListObjectsCommand } from '@aws-sdk/client-s3';
-import { S3_BUCKET } from '$env/static/private';
-import { s3 } from '$lib/s3.server';
+import { S3 } from '$lib/s3.server';
 import { error } from '@sveltejs/kit';
 
 export const load = async () => {
-	const command = new ListObjectsCommand({
-		Bucket: S3_BUCKET
-	});
-	const response = await s3.send(command);
+	const xml = await S3.listObjects();
 
-	let items: { username: string; date: string; url: string }[] = [];
+	const keys: string[] = [];
+	const keyRegex = /<Key>(.*?)<\/Key>/g;
+	let match;
+	while ((match = keyRegex.exec(xml)) !== null) {
+		keys.push(match[1]);
+	}
 
-	response.Contents?.forEach((item) => {
-		if (item.Key) {
-			const splitteed = item.Key.split('-');
-			const username = splitteed[0];
-			const date = splitteed
-				.slice(1)
-				.join('-')
-				.split('.')[0]
-				.replace(/T(\d{2})-(\d{2})-(\d{2})-(\d{1,})Z$/, 'T$1:$2:$3.$4Z');
-			const url = `https://dvr-storage.vrc.bz/${item.Key}`;
+	const items: { username: string; date: string; url: string }[] = [];
 
-			items.push({ username, date, url });
-		}
+	keys.forEach((key) => {
+		const splitteed = key.split('-');
+		const username = splitteed[0];
+		const date = splitteed
+			.slice(1)
+			.join('-')
+			.split('.')[0]
+			.replace(/T(\d{2})-(\d{2})-(\d{2})-(\d{1,})Z$/, 'T$1:$2:$3.$4Z');
+		const url = `https://dvr-storage.vrc.bz/${key}`;
+
+		items.push({ username, date, url });
 	});
 
 	if (items.length === 0) {
